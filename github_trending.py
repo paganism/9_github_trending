12c16b2e8224470cacf08af49559d1a668cbe5ad
yuriy_beloff <paganismrus@gmail.com>
2018-04-07 16:57:53 +0300

    added function get_repos_and_issues

--- github_trending.py
+++ github_trending.py
@@ -3,23 +3,45 @@ import json
 from datetime import datetime, timedelta
 
 
-def get_trending_repositories(top_size, days_in_week):
-    last_week = (datetime.today() - timedelta(days_in_week)).strftime("%Y-%m-%d")
-    params = {'q': 'created>{}'.format(last_week), 'sort': 'stars', 'order': 'desc'}
-    response = requests.get('https://api.github.com/search/repositories', params=params).json()
-    trend_repo = response["items"][:top_size]
+def get_trending_repositories(top_size, count_of_days):
+    days_ago = (datetime.today() -
+                timedelta(count_of_days)).strftime('%Y-%m-%d')
+    params = {'q': 'created>{}'.format(days_ago),
+              'sort': 'stars', 'order': 'desc'}
+    response = requests.get('https://api.github.com/search/repositories',
+                            params=params).json()
+    trend_repo = response['items'][:top_size]
     return trend_repo
 
 
-def repo_info_view(repos):
+def get_repos_and_issues(repos):
+    list_of_repos = []
     for repo in repos:
-        print('name: {}'.format(repo["name"]))
-        print('open_issues: {}'.format(repo["open_issues"]))
-        print('html_url {}'.format(repo["html_url"]))
+        owner = repo['owner']['login']
+        name = repo['name']
+        html_url = repo['html_url']
+        response = requests.get('https://api.github.com/repos/{}/{}/issues'.
+                                format(owner, name)).json()
+        issues_qty = len(response)
+        list_of_repos.append({'owner': owner,
+                              'name': name,
+                              'html_url': html_url,
+                              'count_of_issues': issues_qty
+                              })
+    return list_of_repos
+
+
+def print_repo_info(repos):
+    for repo in repos:
+        print('owner: {}'.format(repo['owner']))
+        print('name: {}'.format(repo['name']))
+        print('open_issues: {}'.format(repo['count_of_issues']))
+        print('html_url {}'.format(repo['html_url']))
 
 
 if __name__ == '__main__':
     top_size = 20
-    days_in_week = 7
-    repos = get_trending_repositories(top_size, days_in_week)
-    repo_info_view(repos)
+    count_of_days = 7
+    trending_repos = get_trending_repositories(top_size, count_of_days)
+    repos_and_issues = get_repos_and_issues(trending_repos)
+    print_repo_info(repos_and_issues)
